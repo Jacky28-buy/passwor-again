90d7f3f6bd607edb011e7f26d2079191c35b0332
Jacky Bai <chujiebai@gmail.com>
2020-01-29 15:41:43 +0800

    improvement

--- password.py
+++ password.py
@@ -1,16 +1,16 @@
 x = 3
-
+password = 'kkbao'
 
 
 while True:
 	pw = input('请输入密码')
 	x = x - 1
-	if pw != 'kkbao':
+	if pw != password:
 		if x != 0:
 			print('密码错误！还有', x , '次机会')
-		if x == 0:
+		else:
 			print('没机会了')
 			break
-	if pw == 'kkbao':
+	if pw == password:
 		print('密码正确')
 		break
